20aee2a119874e30e5c0ef6f387e850183e29bff
rweilbacher <roland.weilbacher@gmai.com>
2022-11-06 12:58:35 +0100

    Remove duplicate spaces from yt transcript

--- obsidian/youtube-transcript-formatter.py
+++ obsidian/youtube-transcript-formatter.py
@@ -4,6 +4,7 @@ import re
 win32clipboard.OpenClipboard()
 text = win32clipboard.GetClipboardData()
 formatted = re.sub("(\r\n|^)[0-9:]*\r\n", " ", text)
+formatted = re.sub(" +", " ", formatted)
 win32clipboard.EmptyClipboard() # Clear the HTML clipboard
 win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, formatted)
 win32clipboard.CloseClipboard()
